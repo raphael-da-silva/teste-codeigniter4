89ff97b5fc5ef9b3c2f55f6809b8691754aa4281
Raphael da Silva <raphael_correa_silva@hotmail.com>
2024-01-04 21:30:47 -0300

    Implementação da busca de endereço por CEP

--- app/Views/client-form.php
+++ app/Views/client-form.php
@@ -20,36 +20,53 @@
     $address = $client->address ?? null;
 ?>
 
-<div class="bg-info p-2">
-    <form action="/submit" method="post">
-        <?php if(isset($client)){ ?>
-            <input type="hidden" name="id" value="<?php echo $client->id; ?>">
-        <?php } ?>
+<div class="row">
+    <div class="bg-info p-2 col">
+        <form action="/submit" method="post">
+            <?php if(isset($client)){ ?>
+                <input type="hidden" name="id" value="<?php echo $client->id; ?>">
+            <?php } ?>
 
-        <div class="mb-2">
-            <label>Nome:</label>
-            <input type="text" name="name" class="form-control" value="<?php echo set_value('name', $name); ?>">
-        </div>
+            <div class="mb-2">
+                <label>Nome:</label>
+                <input type="text" name="name" class="form-control" value="<?php echo set_value('name', $name); ?>">
+            </div>
 
-        <div class="mb-2">
-            <label>E-mail:</label>
-            <input type="email" name="email" class="form-control" value="<?php echo set_value('email', $email); ?>">
-        </div>
+            <div class="mb-2">
+                <label>E-mail:</label>
+                <input type="email" name="email" class="form-control" value="<?php echo set_value('email', $email); ?>">
+            </div>
 
-        <div class="mb-2">
-            <label>Telefone:</label>
-            <input type="phone" name="phone" class="form-control" value="<?php echo set_value('phone', $phone); ?>">
-        </div>
+            <div class="mb-2">
+                <label>Telefone:</label>
+                <input type="phone" name="phone" id="phone" class="form-control" value="<?php echo set_value('phone', $phone); ?>">
+            </div>
 
-        <div class="">
-            <label>Endereço:</label>
-            <input type="text" name="address" class="form-control" value="<?php echo set_value('address', $address); ?>">
-        </div>
+            <div class="">
+                <label>Endereço:</label>
+                <input type="text" name="address" id="address" class="form-control" value="<?php echo set_value('address', $address); ?>">
+            </div>
+
+            <hr>
+
+            <input type="submit" class="btn btn-success btn-lg" value="Salvar">
+        </form>
+    </div>
+
+    <div class="col-4 p-3 bg-dark text-white">
+        <h4>
+            Buscar endereço por CEP
+        </h4>
 
         <hr>
 
-        <input type="submit" class="btn btn-success btn-lg" value="Salvar">
-    </form>
+        <form>
+            <label>Digite o CEP:</label>
+            <input type="text" id="CEP-search" class="form-control">
+
+            <div class="btn btn-warning d-block mt-1" id="search">buscar</div>
+        </form>
+    </div>
 </div>
 
 <?= $this->endSection() ?>
--- app/Views/default.php
+++ app/Views/default.php
@@ -6,13 +6,44 @@
     <link rel="shortcut icon" type="image/png" href="/favicon.ico">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.10.3/jquery.mask.min.js"></script>
 
     <style>
         .content{
             max-width: 1024px;
         }
     </style>
+
+    <script>
+        var GetAddressByCEP = function(CEP){
+            $.getJSON("https://viacep.com.br/ws/"+ CEP +"/json/?callback=?", function(result){
+                if ("erro" in result){
+                    alert('Não foi possivel encontrar o endereço');
+                    return;
+                }
+
+                var addressString = result.logradouro + ' - bairro ' + result.bairro + ' - ' + result.uf;
+                $('#address').val(addressString);
+            });
+        };
+
+        var SearchAddress = function(){
+            $('#search').on('click', function(){
+                var CEP = $('#CEP-search').val();
+                GetAddressByCEP(CEP);
+            });
+        };
+
+        $(function($){
+            $('#phone').mask('(00) 0 0000-0000', {reverse: true});
+            $('#CEP-search').mask('00000000', {reverse: true});
+
+            SearchAddress();
+        });
+    </script>
 </head>
 
 <body>
